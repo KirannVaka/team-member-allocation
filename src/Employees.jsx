1dc4e6a4c176c692a3dc97939d851a6274eb80b1
Kiran Vaka <saikirann.vaka@gmail.com>
2022-10-11 23:36:06 +0800

    employee card template created and rendered data.

--- src/Employees.jsx
+++ src/Employees.jsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import femaleProfile from "./images/femaleProfile.jpg";
+import maleProfile from "./images/maleProfile.jpg";
 
 const Employees = () => {
   const [employees, setEmployees] = useState([
@@ -88,7 +90,31 @@ const Employees = () => {
     },
   ]);
 
-  return <main></main>;
+  return (
+    <main className="container">
+      <div className="row justify-content-center mt-3 mb-3">
+        <div className="col-8">
+          <div className="card-collection">
+            {employees.map((employee) => (
+              <div
+                id={employee.id}
+                className="card m-2"
+                style={{ cursor: "pointer" }}
+              >
+                <img src={femaleProfile} className="card-img-top" />
+                <div className="card-body">
+                  <h5 className="card-tirle">Full Name: {employee.fullName}</h5>
+                  <p className="card-text">
+                    <b>Designation:</b> {employee.designation}
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Employees;
