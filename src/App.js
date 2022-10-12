e21ae744186588c09f940ec41966bb724ae59e6a
Kiran Vaka <saikirann.vaka@gmail.com>
2022-10-12 11:45:38 +0800

    implemented local storage to hold data in bowser.

--- src/App.js
+++ src/App.js
@@ -1,98 +1,102 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Employees from "./Employees";
 import Footer from "./Footer";
 import Header from "./Header";
 
 function App() {
-  const [selectedTeam, setTeam] = useState("TeamA");
+  const [selectedTeam, setTeam] = useState(
+    JSON.parse(localStorage.getItem("selectedTeam")) || "TeamA"
+  );
 
-  const [employees, setEmployees] = useState([
-    {
-      id: 1,
-      fullName: "Bob Jones",
-      designation: "JavaScript Developer",
-      gender: "male",
-      teamName: "TeamA",
-    },
-    {
-      id: 2,
-      fullName: "Jill Bailey",
-      designation: "Node Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 3,
-      fullName: "Gail Shepherd",
-      designation: "Java Developer",
-      gender: "female",
-      teamName: "TeamA",
-    },
-    {
-      id: 4,
-      fullName: "Sam Reynolds",
-      designation: "React Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 5,
-      fullName: "David Henry",
-      designation: "DotNet Developer",
-      gender: "male",
-      teamName: "TeamB",
-    },
-    {
-      id: 6,
-      fullName: "Sarah Blake",
-      designation: "SQL Server DBA",
-      gender: "female",
-      teamName: "TeamB",
-    },
-    {
-      id: 7,
-      fullName: "James Bennet",
-      designation: "Angular Developer",
-      gender: "male",
-      teamName: "TeamC",
-    },
-    {
-      id: 8,
-      fullName: "Jessica Faye",
-      designation: "API Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 9,
-      fullName: "Lita Stone",
-      designation: "C++ Developer",
-      gender: "female",
-      teamName: "TeamC",
-    },
-    {
-      id: 10,
-      fullName: "Daniel Young",
-      designation: "Python Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 11,
-      fullName: "Adrian Jacobs",
-      designation: "Vue Developer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-    {
-      id: 12,
-      fullName: "Devin Monroe",
-      designation: "Graphic Designer",
-      gender: "male",
-      teamName: "TeamD",
-    },
-  ]);
+  const [employees, setEmployees] = useState(
+    JSON.parse(localStorage.getItem("employeesList")) || [
+      {
+        id: 1,
+        fullName: "Bob Jones",
+        designation: "JavaScript Developer",
+        gender: "male",
+        teamName: "TeamA",
+      },
+      {
+        id: 2,
+        fullName: "Jill Bailey",
+        designation: "Node Developer",
+        gender: "female",
+        teamName: "TeamA",
+      },
+      {
+        id: 3,
+        fullName: "Gail Shepherd",
+        designation: "Java Developer",
+        gender: "female",
+        teamName: "TeamA",
+      },
+      {
+        id: 4,
+        fullName: "Sam Reynolds",
+        designation: "React Developer",
+        gender: "male",
+        teamName: "TeamB",
+      },
+      {
+        id: 5,
+        fullName: "David Henry",
+        designation: "DotNet Developer",
+        gender: "male",
+        teamName: "TeamB",
+      },
+      {
+        id: 6,
+        fullName: "Sarah Blake",
+        designation: "SQL Server DBA",
+        gender: "female",
+        teamName: "TeamB",
+      },
+      {
+        id: 7,
+        fullName: "James Bennet",
+        designation: "Angular Developer",
+        gender: "male",
+        teamName: "TeamC",
+      },
+      {
+        id: 8,
+        fullName: "Jessica Faye",
+        designation: "API Developer",
+        gender: "female",
+        teamName: "TeamC",
+      },
+      {
+        id: 9,
+        fullName: "Lita Stone",
+        designation: "C++ Developer",
+        gender: "female",
+        teamName: "TeamC",
+      },
+      {
+        id: 10,
+        fullName: "Daniel Young",
+        designation: "Python Developer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+      {
+        id: 11,
+        fullName: "Adrian Jacobs",
+        designation: "Vue Developer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+      {
+        id: 12,
+        fullName: "Devin Monroe",
+        designation: "Graphic Designer",
+        gender: "male",
+        teamName: "TeamD",
+      },
+    ]
+  );
 
   const handleTeamSelectionChange = ({ target }) => {
     setTeam(target.value);
@@ -108,6 +112,14 @@ function App() {
     );
     setEmployees(updatedEmployees);
   };
+
+  useEffect(() => {
+    localStorage.setItem("employeesList", JSON.stringify(employees));
+  }, [employees]);
+
+  useEffect(() => {
+    localStorage.setItem("selectedTeam", JSON.stringify(selectedTeam));
+  }, [selectedTeam]);
 
   return (
     <div>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,7 +1,13 @@
 const Footer = () => {
+  let today = new Date();
+
   return (
-    <footer>
-      <h1>Footer</h1>
+    <footer className="container">
+      <div className="row justify-content-center mt-3 mb-4">
+        <div className="col-8">
+          <h5>Team Member Allocation App - {today.getFullYear()}</h5>
+        </div>
+      </div>
     </footer>
   );
 };
