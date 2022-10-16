9b6edbe1d94fc03e3bb18c8b36a734539073cc6c
Kiran Vaka <saikirann.vaka@gmail.com>
2022-10-16 18:06:00 +0800

    updated nav links to wrok with gh-pages

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -5,12 +5,15 @@ function Nav() {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <Link className="nav-link" to="/">
+          <Link className="nav-link" to="/team-member-allocation">
             Home
           </Link>
         </li>
         <li>
-          <Link className="nav-link" to="/GroupedTeamMembers">
+          <Link
+            className="nav-link"
+            to="team-member-allocation/GroupedTeamMembers"
+          >
             Team
           </Link>
         </li>
